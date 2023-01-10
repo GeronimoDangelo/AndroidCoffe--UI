06e2d1a0e67ec537e0c85cefb986b44d02d50cc4
Dario DAngelo <dariodangelo21@gmail.com>
2023-01-10 09:04:39 -0300

    2 - Final Version of The app

--- app/src/main/java/com/example/bookapp/data/local/BookDatabase.kt
+++ app/src/main/java/com/example/bookapp/data/local/BookDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.bookapp.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.bookapp.data.local.dao.*
@@ -17,6 +19,21 @@ import com.example.bookapp.domain.model.*
 )
 @TypeConverters(DatabaseConverter::class)
 abstract class BookDatabase : RoomDatabase() {
+
+
+    companion object {
+        fun create(context: Context, useInMemory: Boolean): BookDatabase {
+            val databaseBuilder = if (useInMemory) {
+                Room.inMemoryDatabaseBuilder(context, BookDatabase::class.java)
+            } else {
+                Room.databaseBuilder(context, BookDatabase::class.java, "test_database.db")
+            }
+            return databaseBuilder
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
+
 
     abstract fun bookDao(): BookDao
     abstract fun bookRemoteKeysDao(): BookRemoteKeysDao
